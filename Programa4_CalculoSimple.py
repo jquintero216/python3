66c0b6eb545ad278761ce1635a38432b010e8316
Jose A. Quintero <54563501+jquintero216@users.noreply.github.com>
2020-05-01 15:21:33 -0500

    Add files via upload
    Programa ejemplo 4 Calculo Simple con entrada de datos

---
+++ Programa4_CalculoSimple.py
@@ -0,0 +1,30 @@
+# Lenguaje: Python3
+# Compilador: Python 3.6.9
+# Empresa: N/A
+# Ubicación: Lima - Perú
+
+# Aplicación: Operaciones entre variables ingresadas
+# Versión: 1.0
+# Fecha: abril/27/2020
+
+# Programa 4 Cálculo simple
+# SO: Linux Ubuntu 18.04
+
+Varx = int(input("Introduzca valor variable x: "))
+Vary = int(input("Introduzca valor variable y: "))
+
+# Operacion entre variable
+
+suma = Varx + Vary
+resta = Varx - Vary
+producto = Varx * Vary
+division = Varx / Vary
+mod = Varx % Vary
+
+# Mostrar resultados
+
+print ("Suma: ",suma)
+print ("Resta: ",resta)
+print ("Producto: ",producto)
+print ("Division: ",division)
+print ("Resto: ", mod)
